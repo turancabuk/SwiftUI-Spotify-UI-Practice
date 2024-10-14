412fa8f2ea01a8fc3ed2e769fe5bde373b22108b
turan cabuk <turan6677@gmail.com>
2024-10-14 13:18:17 +0300

    Initial Commit

---
+++ SwiftUI-Spotify-UI-Practice/SwiftUI_Spotify_UI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Spotify_UI_PracticeApp.swift
+//  SwiftUI-Spotify-UI-Practice
+//
+//  Created by Turan Çabuk on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Spotify_UI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
